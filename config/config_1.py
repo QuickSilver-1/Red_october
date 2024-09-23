559dacb1dfe50e5da926b8e30dd5dc5fcf8040df
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2024-09-23 15:46:21 +0300

    Update config_1.py

--- config/config_1.py
+++ config/config_1.py
@@ -5,6 +5,6 @@ PGUSER = 'roman'
 PGPASSWORD = '1234'
 DATABASE = 'october'
 
-POSTGRES_URL = f'postgresql+asyncpg://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
+POSTGRES_URL = f'postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
 
 SECRET_KEY = b'rHOPsLChNAHn_h4LyqfT6kHMUVcnWur1FPMVh3W1vj4='
