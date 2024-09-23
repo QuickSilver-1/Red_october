e1404a09e14c0349fb9ace5d6c04877f33c0d6ae
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2024-09-23 14:40:41 +0300

    Update config_1.py

--- config/config_1.py
+++ config/config_1.py
@@ -1,10 +1,10 @@
 TOKEN = "7422154266:AAFn3WlIlsrJPduTKo2aHzNwJdKnBP0SUok"
 
-ip = '127.0.0.1'
+ip = '185.218.0.57'
 PGUSER = 'roman'
 PGPASSWORD = '1234'
 DATABASE = 'october'
 
-POSTGRES_URL = f'postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
+POSTGRES_URL = f'postgresql+asyncpg://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}'
 
 SECRET_KEY = b'rHOPsLChNAHn_h4LyqfT6kHMUVcnWur1FPMVh3W1vj4='
