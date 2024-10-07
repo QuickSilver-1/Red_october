57655f6d97c3fe1a3e184dab1eb424c66d3b3df7
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2024-10-07 19:31:05 +0300

    Update bitrix.py

--- bitrix.py
+++ bitrix.py
@@ -1,27 +1,39 @@
 from time import sleep
-from requests import get
+import requests
 import openpyxl
 from base64 import b64encode
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 
 def request():
     file_str = ""
+    
+    session = requests.Session()
+
+    retries = Retry(total=5,
+                backoff_factor=0.1,
+                status_forcelist=[500, 502, 503, 504],
+                allowed_methods=frozenset(['GET', 'POST']))
+
+    session.mount('http://', HTTPAdapter(max_retries=retries))
+    session.mount('https://', HTTPAdapter(max_retries=retries))
 
     wb = openpyxl.load_workbook("intimissimi.xlsx")
     for i in wb['Sheet1'].values:
         file_str += ",".join([str(j) for j in i]) + "\n"
 
-    get(url="https://b24-zlccg7.bitrix24.ru/rest/1/h976b7yi1427d5lm/disk.file.uploadversion.json", params={"id": 97, "fileContent": b64encode(file_str.encode("UTF-8")).decode()})
+    x = requests.get(url="https://vmesteplus.bitrix24.ru/rest/18/b51j140hlvfrfwwu//disk.file.uploadversion.json", params={"id": 2520, "fileContent": b64encode(file_str.encode("UTF-8")).decode()})
     file_str = ""
 
     wb = openpyxl.load_workbook("intersharm.xlsx")
     for i in wb['Sheet1'].values:
         file_str += ",".join([str(j) for j in i]) + "\n"
 
-    get(url="https://b24-zlccg7.bitrix24.ru/rest/1/h976b7yi1427d5lm/disk.file.uploadversion.json", params={"id": 95, "fileContent": b64encode(file_str.encode("UTF-8")).decode()})
+    y = requests.get(url="https://vmesteplus.bitrix24.ru/rest/18/b51j140hlvfrfwwu/disk.file.uploadversion.json", params={"id": 4494, "fileContent": b64encode(file_str.encode("UTF-8")).decode()})
+    print(x, y)
 
 while True:
     request()
-    print(1)
     sleep(300)
     
